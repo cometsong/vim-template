7e6472a111e35ba9de3f3af8d29c6840e2112f36
cometsong <benjamin@cometsong.net>
2015-10-14 11:05:01 -0400

    alter templates: add to py, rm bash/sh b/c using bash-support plug

--- templates/=template=.py
+++ templates/=template=.py
@@ -1,11 +1,28 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-#
-# Copyright © %YEAR% %USER% <%MAIL%>
-#
-# Distributed under terms of the %LICENSE% license.
 
 """
 %HERE%
 """
+
+PROJECT_NAME = ''
+VERSION = (0, 1, 'dev') # remove 'dev' from list for full release
+RELEASE_DATE = '%DATE%'
+__author__ = '$USER%'
+__contact__ = '%EMAIL%'
+__license__ = '%LICENSE%'
+__copyright__ = ' '.join((RELEASE_DATE[0:4], __author__, '')) + ''.join(('<', __contact__, '>'))
+__version__ = '.'.join(map(str, VERSION[0:2])) + ''.join(VERSION[2:])
+
+
+""" module layout """
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Core ~~ Imports ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Third Party ~~ Imports ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Specific ~~ Imports ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Exception Classes ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Interface Functions ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Classes ~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Internal Functions & Classes ~~~~"
